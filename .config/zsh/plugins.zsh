e94d846243e4c100cbde29c98bc92719cad06244
Nathan Abeje <nathanmelkau@gmail.com>
2024-02-25 00:31:35 +0100

    plugins for zsh

---
+++ .config/zsh/plugins.zsh
@@ -0,0 +1,8 @@
+
+# plugins
+PLUGIN_DIR="$HOME/.config/zsh/plugins"
+source "$PLUGIN_DIR/zsh-autocomplete/zsh-autocomplete.plugin.zsh"
+source "$PLUGIN_DIR/zsh-autosuggestions/zsh-autosuggestions.zsh"
+source "$PLUGIN_DIR/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+source /usr/share/fzf/completion.zsh
+source /usr/share/fzf/key-bindings.zsh
